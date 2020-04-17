160c711897157e85c4f27a12c7a8bb37f8475a02
FrancoisLZ <63416929+FrancoisLZ@users.noreply.github.com>
2020-04-18 00:09:24 +0200

    Update unittests_François_Louguet.py

--- TDD/unittests_François_Louguet.py
+++ TDD/unittests_François_Louguet.py
@@ -1,21 +1,26 @@
-import Class.py 
+import Class
 import unittest
 
 class get_colorTest(unittest.TestCase):
     
-    def setUp(self):
-        self.color = "jaune"
-        self.brand = "lamborghini"
-        self.price = 500000
+   
     
     def test_get_color(self):
         """ teste le fonctionnement de get_color"""
-        self.assertEqual(get_color(self),self.color)
+        v=Vehicule("jaune","lamborghini",500000)
+        self.assertEqual(Class.get_color(v),v.color)
 
     def test_get_brand(self):
         """ teste le fonctionnement de get_brand"""
-        self.assertEqual(get_brand(self),self.brand)
+        v=Vehicule("jaune","lamborghini",500000)
+        self.assertEqual(Class.get_brand(v),v.brand)
     
     def test_get_price(self):
         """ teste le fonctionnement de get_price"""
-        self.assertEqual(get_price(self),self.price)
+        v=Vehicule("jaune","lamborghini",500000)
+        self.assertEqual(Class.get_price(v),v.price)
+
+    def test_get_width(self):
+        """ teste le fonctionnement de get_width"""
+        c=Car(2,3)
+        self.assertEqual(Class.get_price(c),c.price)
