e6dfecb480f29573d673b14b6e56591f97aa60ca
FrancoisLZ <63416929+FrancoisLZ@users.noreply.github.com>
2020-04-17 23:51:09 +0200

    Update unittests_François_Louguet.py

--- TDD/unittests_François_Louguet.py
+++ TDD/unittests_François_Louguet.py
@@ -1,7 +1,7 @@
-
+import Class.py 
 import unittest
 
-class get_colorTest(unittest.Testcase):
+class get_colorTest(unittest.TestCase):
     
     def setUp(self):
         self.color = "jaune"
@@ -9,5 +9,13 @@ class get_colorTest(unittest.Testcase):
         self.price = 500000
     
     def test_get_color(self):
+        """ teste le fonctionnement de get_color"""
         self.assertEqual(get_color(self),self.color)
 
+    def test_get_color(self):
+        """ teste le fonctionnement de get_color"""
+        self.assertEqual(get_color(self),self.color)
+    
+    def test_get_color(self):
+        """ teste le fonctionnement de get_color"""
+        self.assertEqual(get_color(self),self.color)
