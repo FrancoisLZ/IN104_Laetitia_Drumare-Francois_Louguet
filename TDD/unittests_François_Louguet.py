16a4a1e57cdd834771414bb1886578bed6cb2dc5
FrancoisLZ <63416929+FrancoisLZ@users.noreply.github.com>
2020-04-18 11:44:32 +0200

    Update unittests_François_Louguet.py

--- TDD/unittests_François_Louguet.py
+++ TDD/unittests_François_Louguet.py
@@ -1,26 +1,34 @@
-import Class
 import unittest
 
-class get_colorTest(unittest.TestCase):
+from Class import get_color
+from Class import get_brand
+from Class import get_price
+from Class import get_width
+
+
+class FonctionsTest(unittest.TestCase):
     
    
     
     def test_get_color(self):
         """ teste le fonctionnement de get_color"""
         v=Vehicule("jaune","lamborghini",500000)
-        self.assertEqual(Class.get_color(v),v.color)
+        self.assertEqual(get_color(v),v.color)
 
     def test_get_brand(self):
         """ teste le fonctionnement de get_brand"""
         v=Vehicule("jaune","lamborghini",500000)
-        self.assertEqual(Class.get_brand(v),v.brand)
+        self.assertEqual(get_brand(v),v.brand)
     
     def test_get_price(self):
         """ teste le fonctionnement de get_price"""
         v=Vehicule("jaune","lamborghini",500000)
-        self.assertEqual(Class.get_price(v),v.price)
+        self.assertEqual(get_price(v),v.price)
 
     def test_get_width(self):
         """ teste le fonctionnement de get_width"""
         c=Car(2,3)
-        self.assertEqual(Class.get_price(c),c.price)
+        self.assertEqual(get_price(c),c.price)
+
+if __name__=="__main__":
+    unittest.main()
