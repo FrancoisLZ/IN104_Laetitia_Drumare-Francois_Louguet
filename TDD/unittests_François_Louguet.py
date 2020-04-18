8f9cdf64df916f1bdde3f92b4b9a84aede7fdb3a
FrancoisLZ <63416929+FrancoisLZ@users.noreply.github.com>
2020-04-18 11:45:45 +0200

    Update unittests_François_Louguet.py

--- TDD/unittests_François_Louguet.py
+++ TDD/unittests_François_Louguet.py
@@ -13,22 +13,22 @@ class FonctionsTest(unittest.TestCase):
     def test_get_color(self):
         """ teste le fonctionnement de get_color"""
         v=Vehicule("jaune","lamborghini",500000)
-        self.assertEqual(get_color(v),v.color)
+        self.assertEqual(get_color(v),"jaune")
 
     def test_get_brand(self):
         """ teste le fonctionnement de get_brand"""
         v=Vehicule("jaune","lamborghini",500000)
-        self.assertEqual(get_brand(v),v.brand)
+        self.assertEqual(get_brand(v),"lamborghini")
     
     def test_get_price(self):
         """ teste le fonctionnement de get_price"""
         v=Vehicule("jaune","lamborghini",500000)
-        self.assertEqual(get_price(v),v.price)
+        self.assertEqual(get_price(v),500000)
 
     def test_get_width(self):
         """ teste le fonctionnement de get_width"""
         c=Car(2,3)
-        self.assertEqual(get_price(c),c.price)
+        self.assertEqual(get_width(c),3)
 
 if __name__=="__main__":
     unittest.main()
